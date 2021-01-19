e5dff4b651659b2e0ed799dccc02e9981b51fcaf
darwisy327 <77084832+darwisy327@users.noreply.github.com>
2021-01-19 09:01:46 +0700

    Dikasi penutup
    Ditambah penutup titik koma
    Ada baris tambahan

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
    public static main(){
     System.out.println("Hello World");
-      System.out.println("Selamat Berliburan :)")
+      System.out.println("Selamat Berliburan :)");
+         System.out.println("Ape buat nih mad?");
          
     }
   }
