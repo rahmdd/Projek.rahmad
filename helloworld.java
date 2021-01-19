69879b4d8b8d860831c91dda0f39923c7555f2ae
rahmdd <77084937+rahmdd@users.noreply.github.com>
2021-01-19 09:28:29 +0700

    Update Selamat pagi
    Saya menambahkan tulisan "Selamat pagi"

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static main(){
     System.out.println("Hello World");
       System.out.println("Selamat Berliburan :)");
-         System.out.println("Ape buat nih mad?");
-         
+         System.out.println("Selamat pagi");
+         System.out.println("");
     }
   }
